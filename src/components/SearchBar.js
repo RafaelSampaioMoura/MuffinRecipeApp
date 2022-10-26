853ce1223a71faaaaa1ca367a5f66afc3cf8c1c7
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-10-26 18:52:39 -0300

    Requisito 11 completo

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,54 @@
+import React, { useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
+
 function SearchBar() {
+  const { setRadio, apiMeals } = useContext(RecipesContext);
+
+  const handleChange = ({ target }) => {
+    setRadio(target.value);
+  };
+
+  const onClick = () => {
+    apiMeals();
+  };
+
   return (
     <div>
       <label htmlFor="ingredient-search-radio">
         Ingredient
         <input
           type="radio"
-          name="ingredient-search-radio"
+          name="escolha"
+          value="ingredient"
           data-testid="ingredient-search-radio"
+          onChange={ handleChange }
         />
       </label>
       <label htmlFor="name-search-radio">
         Name
         <input
           type="radio"
-          name="name-search-radio"
+          name="escolha"
+          value="name"
           data-testid="name-search-radio"
+          onChange={ handleChange }
         />
       </label>
       <label htmlFor="first-letter-search-radio">
         First letter
         <input
           type="radio"
-          name="first-letter-search-radio"
+          name="escolha"
+          value="first-letter"
           data-testid="first-letter-search-radio"
+          onChange={ handleChange }
         />
       </label>
       <button
         type="button"
         name="exec-search-btn"
         data-testid="exec-search-btn"
+        onClick={ onClick }
       >
         Search
       </button>
